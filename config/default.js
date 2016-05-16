e74b8d6e7b2f9160e1ac5f6375ee53736893f926
Kevin McGrath <kevin.mcgrath@sungardas.com>
2016-05-16 12:35:04 -0400

    add travis configuration

--- config/default.js
+++ config/default.js
@@ -1,98 +1,49 @@
 var gitrev = require('git-rev-sync');
 
+var regions = [
+  'us-east-1',
+  'us-west-1',
+  'us-west-2',
+  'eu-west-1',
+  'eu-central-1',
+  'ap-northeast-1',
+  'ap-northeast-2',
+  'ap-southeast-1',
+  'ap-southeast-2',
+  'sa-east-1'
+];
+
+var buckets = [];
+var versionPath = gitrev.branch();
+
+if (process.env.TRAVIS) {
+  if (process.env.TRAVIS_PULL_REQUEST !== "false") {
+    versionPath = ["PR",process.env.TRAVIS_PULL_REQUEST].join('/');
+  }
+  else if (process.env.TRAVIS_TAG) {
+    versionPath = ["release",process.env.TRAVIS_TAG].join('/');
+  }
+  else {
+    versionPath = process.env.TRAVIS_BRANCH;
+  }
+}
+
+regions.forEach(function(regionName) {
+  buckets.push({
+    aws: {
+      region: regionName,
+      bucket: 'condensation-particles.'+regionName
+    },
+    prefix: 'particles-enhanced-snapshots/'+versionPath,
+    clean: true,
+    validate: true,
+    create: true
+  });
+});
+
+
 var config = {
-  s3: [
-    {
-      aws: {
-        region: 'us-east-1',
-        bucket: 'particles-enhanced-snapshots.us-east-1'
-      },
-      prefix: gitrev.branch(),
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'us-west-2',
-        bucket: 'particles-enhanced-snapshots.us-west-2'
-      },
-      prefix: gitrev.branch(),
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'us-west-1',
-        bucket: 'particles-enhanced-snapshots.us-west-1'
-      },
-      prefix: gitrev.branch(),
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'eu-west-1',
-        bucket: 'particles-enhanced-snapshots.eu-west-1'
-      },
-      prefix: gitrev.branch(),
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'eu-central-1',
-        bucket: 'particles-enhanced-snapshots.eu-central-1'
-      },
-      prefix: gitrev.branch(),
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'ap-southeast-1',
-        bucket: 'particles-enhanced-snapshots.ap-southeast-1'
-      },
-      prefix: gitrev.branch(),
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'ap-southeast-2',
-        bucket: 'particles-enhanced-snapshots.ap-southeast-2'
-      },
-      prefix: gitrev.branch(),
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'ap-northeast-1',
-        bucket: 'particles-enhanced-snapshots.ap-northeast-1'
-      },
-      clean: true,
-      prefix: gitrev.branch(),
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'sa-east-1',
-        bucket: 'particles-enhanced-snapshots.sa-east-1'
-      },
-      clean: true,
-      prefix: gitrev.branch(),
-      validate: true,
-      create: true
-    }
-  ],
+  s3: buckets,
   dist: 'dist'
 };
 
