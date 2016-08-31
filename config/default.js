18c396a9c6cfcd307ac8cd04615290556cd3babe
Yuriy Kyrychynskiy <ykyr@softserveinc.com>
2016-08-31 12:44:03 +0000

    Commented unsupported region

--- config/default.js
+++ config/default.js
@@ -4,10 +4,11 @@ var regions = [
   'us-east-1',
   'us-west-1',
   'us-west-2',
-  'eu-west-1'
+  'eu-west-1',
   'eu-central-1',
   'ap-northeast-1',
-  'ap-northeast-2',
+  // TODO: Cannot use until AMI list is updated
+  //'ap-northeast-2',
   'ap-southeast-1',
   'ap-southeast-2',
   'sa-east-1'
