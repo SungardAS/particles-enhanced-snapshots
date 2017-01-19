d6aaa64f706612e5ddcebf5c16a293f4eefcc1a8
Tomasz Wanczyk <twanc@softserveinc.com>
2017-01-19 14:12:58 +0100

    initial: documents and base policy added

--- config/default.js
+++ config/default.js
@@ -2,16 +2,16 @@ var gitrev = require('git-rev-sync');
 
 var regions = [
   'us-east-1',
-  'us-east-2',
-  'us-west-1',
-  'us-west-2',
-  'eu-west-1',
-  'eu-central-1',
-  'ap-northeast-1',
-  'ap-northeast-2',
-  'ap-southeast-1',
-  'ap-southeast-2',
-  'sa-east-1'
+//  'us-east-2',
+//  'us-west-1',
+//  'us-west-2',
+//  'eu-west-1',
+//  'eu-central-1',
+//  'ap-northeast-1',
+//  'ap-northeast-2',
+//  'ap-southeast-1',
+//  'ap-southeast-2',
+//  'sa-east-1'
 ];
 
 var buckets = [];
